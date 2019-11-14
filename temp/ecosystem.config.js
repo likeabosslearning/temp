5be3bef23db3bb25e11621a0811013648aa80358
Edison Garcia <egachi@live.com>
2019-11-14 15:01:13 -0600

    Updating

--- temp/ecosystem.config.js
+++ temp/ecosystem.config.js
@@ -4,12 +4,6 @@ module.exports = {
     exec_interpreter: "./node_modules/.bin/babel-node",
     error_file: 'err.log',
     out_file: 'out.log',
-    log_file: 'combined.log',
-    env: {
-      NODE_ENV: "development",
-    },
-    env_production: {
-      NODE_ENV: "production",
-    }
+    log_file: 'combined.log'
   }]
 }
