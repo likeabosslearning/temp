4a59c55b6cd8f7a0a487f7866fc22d1720b2488c
Edison Garcia <egachi@live.com>
2019-11-14 18:11:46 -0600

    Updating

--- temp/startup.sh
+++ temp/startup.sh
@@ -3,11 +3,10 @@
 cd /home/site/wwwroot
 INSTALL_DIR=/home/site/wwwroot/install.txt
 
-if [-d "$INSTALL_DIR"]; then
+if [ -f "$INSTALL_DIR" ]; then
     echo "Npm was executed"
-
 else
-    echo "Building app"
+    echo "Running npm install & npm run build"
     npm install
     npm run build
     touch install.txt
